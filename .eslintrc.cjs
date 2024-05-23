69a416889c76d4b9a327c4679a3e6ab205ba7406
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-05-23 14:50:22 +0300

    setup configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,138 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-		'airbnb',
-	],
-	ignorePatterns: [
-		'dist',
-		'.eslintrc.cjs',
-		'vite.config.ts',
-		'postcss.config.js',
-		'tailwind.config.js',
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
-	rules: {
-		'react-refresh/only-export-components': [
-			'warn',
-			{ allowConstantExport: true },
-		],
-	},
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+    'airbnb',
+    'airbnb-typescript'
+  ],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    'postcss.config.js',
+    'tailwind.config.js'
+  ],
+  settings: {
+    react: {
+      version: '18'
+    }
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    project: ['tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['react-refresh', 'simple-import-sort'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'linebreak-style': ['error', 'unix'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-filename-extension': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'padding-line-between-statements': 'off',
+    'prefer-const': 'warn',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    'no-tabs': [
+      'error',
+      {
+        allowIndentationTabs: true
+      }
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1
+      }
+    ],
+    'no-var': 'error',
+    'no-console': 'warn',
+    'arrow-parens': ['error', 'always'],
+    'no-else-return': [
+      'error',
+      {
+        allowElseIf: false
+      }
+    ],
+    'jsx-quotes': ['error', 'prefer-double'],
+    semi: ['error', 'always'],
+    'max-params': ['error', 5],
+    'lines-between-class-members': ['error', 'always'],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowTypedFunctionExpressions: true
+      }
+    ],
+    '@typescript-eslint/quotes': ['error', 'single'],
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all'
+      }
+    ],
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      {
+        allowSingleExtends: true
+      }
+    ],
+    '@typescript-eslint/padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: [
+          'switch',
+          'class',
+          'function',
+          'if',
+          'return',
+          'try',
+          'interface',
+          'type'
+        ]
+      }
+    ],
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    'import/no-unresolved': 'off',
+    'import/newline-after-import': [
+      'error',
+      {
+        count: 1
+      }
+    ],
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
+  }
 };
